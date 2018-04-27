3b901012a250877febcdf5b1a8ed619d8fa12ac1
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2018-04-27 19:07:20 -0400

    Loader.py, trainer.py: 1st commit

---
+++ Loader.py
@@ -0,0 +1,129 @@
+# Loader.py
+import argparse
+import torch
+import torchvision
+import PIL
+import io
+import os
+import random
+import numpy
+
+
+"""class SampleImage:
+    def __init__(self, imageID, tensor, label=None):
+        self.imageID = imageID
+        self.tensor = tensor
+        self.label = label
+"""
+
+class Importer:
+    def __init__(self, categoriesDirectory, maximumNumberOfImages=0):
+        # List the categories in the categoriesDirectory
+        directoriesList = [os.path.join(categoriesDirectory, o) for o in os.listdir(categoriesDirectory)
+                           if os.path.isdir(os.path.join(categoriesDirectory, o))]
+        #print ("Import.__init__(): directoriesList =", directoriesList)
+        self.filepathToClassDic = {}
+        self.filepaths = []
+        self.classNames = []
+        for directory in directoriesList:
+            className = os.path.basename(directory)
+            self.classNames.append(className)
+            filepaths = [os.path.join(directory, f) for f in os.listdir(directory)
+                         if os.path.isfile(os.path.join(directory, f))]
+            for filepath in filepaths:
+                self.filepathToClassDic[filepath] = className
+                self.filepaths.append(filepath)
+        print("")
+
+    def ClassIndex(self, className):
+        for classNdx in range(len(self.classNames)):
+            if self.classNames[classNdx] == className:
+                return classNdx
+        raise Exception("Importer.ClassIndex(): Class name {} was not found in the list".format(className))
+
+    def FilepathToClassDic(self):
+        return self.filepathToClassDic
+
+    def SplitForTrainAndValidation(self, numberOfTrainingImages, numberOfValidationImages):
+        trainFilepathToClassDic = {}
+        validationFilepathToClassDic = {}
+        correctedNumberOfTrainingImages = numberOfTrainingImages
+        correctedNumberOfValidationImages = numberOfValidationImages
+        if numberOfTrainingImages + numberOfValidationImages > len(self.filepathToClassDic):
+            correctedNumberOfTrainingImages = int (numberOfTrainingImages * len(self.filepathToClassDic) / \
+                                    (numberOfTrainingImages + numberOfValidationImages) )
+            correctedNumberOfValidationImages = len(self.filepathToClassDic) - correctedNumberOfTrainingImages
+        # Generate a list of random indices
+        indices = random.sample(range(len(self.filepaths)), len(self.filepaths))
+        for trainNdx in range(correctedNumberOfTrainingImages):
+            index = indices[trainNdx]
+            filepath = self.filepaths[index]
+            trainFilepathToClassDic[filepath] = self.filepathToClassDic[filepath]
+        for validationNdx in range(correctedNumberOfTrainingImages, correctedNumberOfTrainingImages + correctedNumberOfValidationImages):
+            index = indices[validationNdx]
+            filepath = self.filepaths[index]
+            validationFilepathToClassDic[filepath] = self.filepathToClassDic[filepath]
+        return trainFilepathToClassDic, validationFilepathToClassDic
+
+    def ConvertToTensors(self, filepathToClassDic, imageSize): # imageSize: (width, height)
+        imagesTensor = torch.FloatTensor(len(filepathToClassDic), 3, imageSize[1], imageSize[0]) # NCHW
+        labelsTensor = torch.LongTensor(len(filepathToClassDic), 1)
+
+        preprocessing = torchvision.transforms.Compose([
+            torchvision.transforms.Resize((imageSize[1], imageSize[0])),  # Resize expects (h, w)
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2))
+        ])
+        filepathNdx = 0
+        for filepath in filepathToClassDic:
+            pilImg = PIL.Image.open(filepath)
+            #pilImg.show()
+            imgTensor = preprocessing(pilImg)
+            tensorShape = imgTensor.shape
+            if imgTensor.shape[0] > 3:
+                imgTensor = imgTensor[0:3]
+            #if tensorShape[0] == 3:
+
+            imagesTensor[filepathNdx] = imgTensor
+
+            className = filepathToClassDic[filepath]
+            classNdx = self.ClassIndex(className)
+            labelsTensor[filepathNdx] = classNdx
+            filepathNdx += 1
+            #else:
+            #    print ("Importer.ConvertToTensors(): The image {} doesn't have 3 channels".format(filepath))
+
+
+        return imagesTensor, labelsTensor
+
+    def MinibatchIndices(self, numberOfSamples, minibatchSize):
+        shuffledList = numpy.arange(numberOfSamples)
+        numpy.random.shuffle(shuffledList)
+        minibatchesIndicesList = []
+        numberOfWholeLists = int(numberOfSamples / minibatchSize)
+        for wholeListNdx in range(numberOfWholeLists):
+            minibatchIndices = shuffledList[wholeListNdx * minibatchSize: (wholeListNdx + 1) * minibatchSize]
+            minibatchesIndicesList.append(minibatchIndices)
+        # Add the last incomplete minibatch
+        if numberOfWholeLists * minibatchSize < numberOfSamples:
+            lastMinibatchIndices = shuffledList[numberOfWholeLists * minibatchSize:]
+            minibatchesIndicesList.append(lastMinibatchIndices)
+        return minibatchesIndicesList
+
+# Tester
+if __name__ == '__main__':
+    print ("Loader.py")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('baseDirectory', help='The directory containing the directories train/ and test/')
+    parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
+
+    args = parser.parse_args()
+    args.cuda = not args.disable_cuda and torch.cuda.is_available()
+
+    importer = Importer(os.path.join(args.baseDirectory, 'train'))
+    filepathToClassDic = importer.FilepathToClassDic()
+    #print ("filepathToClassDic =", filepathToClassDic)
+    print ("len(filepathToClassDic) =",len(filepathToClassDic))
+    trainFilepathToClassDic, validationFilepathToClassDic = importer.SplitForTrainAndValidation(1000, 250)
+    trainImagesTensor, trainLabelsTensor = importer.ConvertToTensors(trainFilepathToClassDic, (256, 256))
+    print ("trainLabelsTensor =", trainLabelsTensor)
---
+++ trainer.py
@@ -0,0 +1,125 @@
+import torch
+import torchvision
+import argparse
+import ast
+import os
+import ConvStackClassifier
+import Loader
+
+print('trainer.py')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('baseDirectory', help='The directory containing the files train.json and validation.json')
+parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
+parser.add_argument('--architecture', help='The neural network architecture (Default: ConvStack_3_3_32_7_2_32_7_2_32_7_2_12_256_0.5)', default='ConvStack_3_3_32_7_2_32_7_2_32_7_2_12_256_0.5')
+parser.add_argument('--restartWithNeuralNetwork', help='Restart the training with this neural network filename')
+parser.add_argument('--lossFunction', help='Loss function (Default: CrossEntropyLoss)', default='CrossEntropyLoss')
+parser.add_argument('--learningRate', help='The learning rate (Default: 0.001)', type=float, default=0.001)
+parser.add_argument('--momentum', help='The learning momentum (Default: 0.9)', type=float, default=0.9)
+parser.add_argument('--imageSize', help='The image size (width, height) (Default: (256, 256))', default='(256, 256)')
+parser.add_argument('--numberOfEpochs', help='Number of epochs (Default: 200)', type=int, default=200)
+parser.add_argument('--minibatchSize', help='Minibatch size (Default: 32)', type=int, default=32)
+parser.add_argument('--numberOfValidationImages', help='The number of images used for validation (Default: 128)', type=int, default=128)
+parser.add_argument('--numberOfTrainingImages', help='The maximum number of training images (Default: 0, which means no limit)', type=int, default=0)
+
+args = parser.parse_args()
+args.cuda = not args.disable_cuda and torch.cuda.is_available()
+
+imageSize = ast.literal_eval(args.imageSize)
+print ("imageSize =", imageSize)
+
+loader = Loader.Importer(args.baseDirectory, args.numberOfTrainingImages + args.numberOfValidationImages)
+trainFilepathToClassDic, validationFilepathToClassDic = loader.SplitForTrainAndValidation(args.numberOfTrainingImages, args.numberOfValidationImages)
+trainFilepaths = [*trainFilepathToClassDic]
+validationFilepaths = [*validationFilepathToClassDic]
+print ("len(trainFilepaths) = {}; len(validationFilepaths) = {}".format(len(trainFilepaths), len(validationFilepaths)))
+#trainImagesTensor, trainLabelsTensor = loader.ConvertToTensors(trainFilepathToClassDic, imageSize)
+
+
+# Create a neural network and an optimizer
+if args.architecture == 'ConvStack_3_3_32_7_2_32_7_2_32_7_2_12_256_0.5':
+    structureElements = ConvStackClassifier.ExtractStructureFromFilename(args.architecture)
+    neuralNet = ConvStackClassifier.NeuralNet(structureElements[2], structureElements[0],
+                                              structureElements[3], structureElements[4],
+                                              structureElements[5])
+    imageSize = (256, 256)
+    # Create an optimizer
+    optimizer = torch.optim.SGD(neuralNet.parameters(), lr=args.learningRate, momentum=args.momentum)
+elif args.architecture == 'ConvStack_3_3_32_7_2_32_7_2_32_7_2_12_128_0.5':
+    structureElements = ConvStackClassifier.ExtractStructureFromFilename(args.architecture)
+    neuralNet = ConvStackClassifier.NeuralNet(structureElements[2], structureElements[0],
+                                              structureElements[3], structureElements[4],
+                                              structureElements[5])
+    imageSize = (128, 128)
+    # Create an optimizer
+    optimizer = torch.optim.SGD(neuralNet.parameters(), lr=args.learningRate, momentum=args.momentum)
+else:
+    raise NotImplementedError("trainer.py: Unsupported architecture '{}'".format(args.architecture))
+
+
+validationImagesTensor, validationLabelsTensor = loader.ConvertToTensors(validationFilepathToClassDic, imageSize)
+# Wrap validation data in Variables, once and for all
+validationImagesTensor = torch.autograd.Variable(validationImagesTensor)
+validationLabelsTensor = torch.autograd.Variable(validationLabelsTensor.squeeze(1))
+
+if args.cuda:
+    neuralNet.cuda()  # Move to GPU
+    validationImagesTensor = validationImagesTensor.cuda()
+    validationLabelsTensor = validationLabelsTensor.cuda()
+
+
+# Create a loss function
+if args.lossFunction == 'CrossEntropyLoss':
+    lossFunction = torch.nn.CrossEntropyLoss()
+else:
+    raise NotImplementedError("trainer.py: Unsupported loss function '{}'".format(args.lossFunction))
+
+
+#logSoftMax = torch.nn.LogSoftmax()
+
+for epoch in range(1, args.numberOfEpochs + 1):
+    print ("\nEpoch", epoch)
+    averageTrainLoss = 0
+    minibatchIndicesListList = loader.MinibatchIndices(len(trainFilepaths), args.minibatchSize)
+    for minibatchListNdx in range(len(minibatchIndicesListList)):
+        print('.', end="", flush=True)  # Print a dot without line return, right now
+        minibatchIndicesList = minibatchIndicesListList[minibatchListNdx]
+        thisMinibatchSize = len(minibatchIndicesList)
+        minibatchFilepathToClassDic = {}
+        for index in minibatchIndicesList:
+            filepath = trainFilepaths[index]
+            minibatchFilepathToClassDic[filepath] = trainFilepathToClassDic[filepath]
+
+        minibatchImgsTensor, minibatchLabelsTensor = loader.ConvertToTensors(minibatchFilepathToClassDic, imageSize)
+
+        # Wrap in Variable
+        minibatchImgsTensor = torch.autograd.Variable(minibatchImgsTensor)
+        minibatchLabelsTensor = torch.autograd.Variable(minibatchLabelsTensor.squeeze(1))
+        if args.cuda:
+            minibatchImgsTensor = minibatchImgsTensor.cuda()
+            minibatchLabelsTensor = minibatchLabelsTensor.cuda()
+
+        # Zero gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        actualOutput = neuralNet(minibatchImgsTensor) # That's where the memory goes up
+
+        # Loss
+        #print (minibatchLabelsTensor)
+        loss = lossFunction(actualOutput, minibatchLabelsTensor)
+        #print ("loss =", loss)
+
+        # Backward pass
+        loss.backward()
+
+        # Parameters update
+        optimizer.step()
+
+        averageTrainLoss += loss.data[0]
+
+    averageTrainLoss = averageTrainLoss / len(minibatchIndicesListList)
+    # Validation
+    validationOutput = neuralNet(validationImagesTensor)
+    validationLoss = lossFunction(validationOutput, validationLabelsTensor)
+    print("\nEpoch {}: Average train loss = {}; validationLoss = {}".format(epoch, averageTrainLoss, validationLoss.data[0]))
